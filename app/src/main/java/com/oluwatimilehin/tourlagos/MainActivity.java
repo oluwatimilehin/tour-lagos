823295831828cb5e3c495fe8ea5f53e48caa96f8
Oluwatimilehin <timad97@Yahoo.com>
2016-11-19 15:46:59 +0100

    Create new activities and add intents to open them

--- app/src/main/java/com/oluwatimilehin/tourlagos/MainActivity.java
+++ app/src/main/java/com/oluwatimilehin/tourlagos/MainActivity.java
@@ -1,7 +1,10 @@
 package com.oluwatimilehin.tourlagos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView attractions = (TextView) findViewById(R.id.attractions);
+        if(attractions != null) {
+            attractions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(MainActivity.this, AttractionsActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+        TextView hotels = (TextView) findViewById(R.id.hotels);
+        if(hotels != null){
+            hotels.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(MainActivity.this, HotelsActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+        TextView rentals = (TextView) findViewById(R.id.rentals);
+        rentals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RentalsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView restaurants = (TextView) findViewById(R.id.restaurants);
+        restaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/oluwatimilehin/tourlagos/RentalsActivity.java
@@ -0,0 +1,13 @@
+package com.oluwatimilehin.tourlagos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RentalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rentals);
+    }
+}
